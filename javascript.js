cb4783d337df0f22b5bd26b75d28f5a1422bf8ff
Elijah <kcerhajile@gmail.com>
2023-08-31 17:23:20 -0400

    Added drawing function, as well as a check for if the mouse is down (noticed it in another users project and loved it so I made one too)

--- javascript.js
+++ javascript.js
@@ -1,13 +1,25 @@
-
-
-
 function fillContainer(size){
     for(let i = 0; i<size*size;i++){const cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.addEventListener('mouseover', draw);
     cell.style.cssText = `height: ${(1/size)*100}%; width: ${(1/size)*100}%`;
     document.querySelector('.container').appendChild(cell);}
-}
+};
 
+function startDrawing(){
+    canDraw = true;
+};
+function stopDrawing(){
+    canDraw = false;    
+};
 
+function draw(e){
+    if(canDraw){this.style.cssText += "background-color: black;"};
+};
+
+window.addEventListener('mousedown', startDrawing);
+window.addEventListener('mouseup', stopDrawing);
+
+let canDraw = false;
 let squareSize = 16;
 fillContainer(squareSize);
