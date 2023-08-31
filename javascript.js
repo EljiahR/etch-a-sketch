5629115440e64723cf10ca41319be993b5557d7e
Elijah <kcerhajile@gmail.com>
2023-08-31 16:51:30 -0400

    Edited fillContainer function to take a variable and use that to decide how many cells and their size to fit container

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
 
 
 
-function fillContainer(){
-    for(let i = 0; i<16*16;i++){const cell = document.createElement('div');
+function fillContainer(size){
+    for(let i = 0; i<size*size;i++){const cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.style.cssText = `height: ${(1/size)*100}%; width: ${(1/size)*100}%`;
     document.querySelector('.container').appendChild(cell);}
 }
 
-fillContainer();
+
+let squareSize = 16;
+fillContainer(squareSize);
