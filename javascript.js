7c027db9cd3ff1dbf825e51d597432899c94da2e
Elijah <kcerhajile@gmail.com>
2023-09-01 15:08:17 -0400

    Made options visible, added color picker, gave up on my etch-a-sketch look idea(for now...)

--- javascript.js
+++ javascript.js
@@ -2,11 +2,13 @@ function fillContainer(size){
     for(let i = 0; i<size*size;i++){const cell = document.createElement('div');
     cell.classList.add('cell');
     cell.addEventListener('mouseover', draw);
+    cell.addEventListener('click', draw);
     cell.style.cssText = `height: ${(1/size)*100}%; width: ${(1/size)*100}%`;
     document.querySelector('.container').appendChild(cell);}
 };
 
 function startDrawing(){
+    
     canDraw = true;
 };
 function stopDrawing(){
@@ -14,12 +16,18 @@ function stopDrawing(){
 };
 
 function draw(e){
-    if(canDraw){this.style.cssText += "background-color: black;"};
+    if(canDraw || e.type == 'click'){this.style.cssText += `background-color: ${drawColor}`;};
+};
+
+function pickColor(e){
+    drawColor = this.value;
 };
 
 window.addEventListener('mousedown', startDrawing);
 window.addEventListener('mouseup', stopDrawing);
+document.querySelector('#color-pick').addEventListener('input', pickColor);
 
 let canDraw = false;
 let squareSize = 16;
+let drawColor = "black";
 fillContainer(squareSize);
